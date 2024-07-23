eb04884b0ce6c0792bcd0d10890557a33bc7ba69
Ilmi <ilmirasyidah.sekawanmedia@gmail.com>
2024-07-23 10:44:53 +0700

    feat: data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config";
+import { Event } from "@/types/event";
+import Link from "next/link";
 
-export default function Home() {
+interface HomeProps {
+  events: Event[];
+}
+
+export default function Home({ events }: HomeProps) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <span className="btn-secondary">View All Events</span>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1, // In seconds
+  };
 }
